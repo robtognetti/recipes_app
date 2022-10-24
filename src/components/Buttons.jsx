e6148f7d79159ecca8d046f6ef7b5fd93ce569fd
Pedroscsilva <pedroscsilva2001@gmail.com>
2022-10-24 19:21:57 -0300

    req 22

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from '../context/AppContext';
 
 export default function Buttons({ categories, type }) {
   const MAX_RENDER = 4;
   const { setDrinksList, setMealList } = useContext(AppContext);
+  const [filteredButton, setFilteredButton] = useState('');
+
+  const resetMealList = async () => {
+    const endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+    const { meals } = await fetch(endpoint).then((response) => response.json());
+    setMealList(meals);
+  };
 
   const changeMealsList = async (innerText) => {
     if (innerText === 'All') {
-      const endpoint = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-      const { meals } = await fetch(endpoint).then((response) => response.json());
-      setMealList(meals);
+      resetMealList();
     } else {
       const endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${innerText}`;
       const { meals } = await fetch(endpoint).then((response) => response.json());
@@ -18,11 +23,15 @@ export default function Buttons({ categories, type }) {
     }
   };
 
+  const resetDrinksList = async () => {
+    const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    const { drinks } = await fetch(endpoint).then((response) => response.json());
+    setDrinksList(drinks);
+  };
+
   const changeDrinksList = async (innerText) => {
     if (innerText === 'All') {
-      const endpoint = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-      const { drinks } = await fetch(endpoint).then((response) => response.json());
-      setDrinksList(drinks);
+      resetDrinksList();
     } else {
       const endpoint = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${innerText}`;
       const { drinks } = await fetch(endpoint).then((response) => response.json());
@@ -35,13 +44,29 @@ export default function Buttons({ categories, type }) {
     switch (switchKey) {
     case 'drinks':
       await changeDrinksList(innerText);
+      setFilteredButton(innerText);
       break;
     case 'meals':
       await changeMealsList(innerText);
+      setFilteredButton(innerText);
       break;
     default:
       break;
     }
+  };
+
+  const cleanFilter = async (switchKey) => {
+    switch (switchKey) {
+    case 'drinks':
+      await resetDrinksList();
+      break;
+    case 'meals':
+      await resetMealList();
+      break;
+    default:
+      break;
+    }
+    setFilteredButton('');
   };
 
   return (
@@ -60,7 +85,11 @@ export default function Buttons({ categories, type }) {
               type="button"
               key={ category.strCategory }
               data-testid={ `${category.strCategory}-category-filter` }
-              onClick={ (e) => handleClick(e, type) }
+              onClick={
+                filteredButton === category.strCategory
+                  ? () => cleanFilter(type)
+                  : (e) => handleClick(e, type)
+              }
             >
               {category.strCategory}
             </button>
