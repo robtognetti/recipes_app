a57d9bd97db6e3ef6f6fefb37e26019c94a17fad
Vinícius Rodrigues <viniciusrodrigues770@gmail.com>
2022-10-24 16:00:32 -0300

    Iniciando o projeto!

---
+++ src/pages/DoneRecipes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DoneRecipes() {
+  return (
+    <div>
+      <h1>Done Recipes</h1>
+    </div>
+  );
+}
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import DoneRecipes from '../pages/DoneRecipes';
+import Drinks from '../pages/Drinks';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
+import Login from '../pages/Login';
+import Meals from '../pages/Meals';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ Meals } />
+        <Route exact path="/drinks" component={ Drinks } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+      </Switch>
+    </BrowserRouter>
+  );
+}
