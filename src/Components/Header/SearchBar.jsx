b1dce64a13b3b6d9e5aab2a528b57f8889daa0eb
Ewerton Ribeiro <ewertondosanjosz@gmail.com>
2022-10-25 10:58:50 -0300

    Requisito 10

--- src/Components/Header/SearchBar.jsx
+++ src/Components/Header/SearchBar.jsx
@@ -2,6 +2,25 @@ import React from 'react';
 
 export default function SearchBar() {
   return (
-    <input data-testid="search-input" type="search" placeholder="Search" />
+    <div>
+      <input data-testid="search-input" type="search" placeholder="Search" />
+      <div>
+        <input
+          type="radio"
+          name="ingredients"
+          data-testid="ingredient-search-radio"
+        />
+        <input type="radio" name="name" data-testid="name-search-radio" />
+        <input
+          type="radio"
+          name="first-letter"
+          data-testid="first-letter-search-radio"
+        />
+      </div>
+
+      <button type="button" data-testid="exec-search-btn">
+        SEARCH
+      </button>
+    </div>
   );
 }
