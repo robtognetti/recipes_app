b7a0dc692430db43b2e3d4446deae8bd42b23c1b
Ewerton Ribeiro <ewertondosanjosz@gmail.com>
2022-10-26 18:17:51 -0300

    Req 57 58 59

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import Footer from '../components/Footer';
+import ProfileImage from '../images/profileIcon.svg';
 
 export default function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+
   return (
-    <>
-      <h1>Profile!</h1>
+    <section>
+      <div>
+        <div>
+          <img src={ ProfileImage } alt="User Profile" />
+          <h1>Profile</h1>
+        </div>
+        <div>
+          <h1 data-testid="profile-email">{email}</h1>
+        </div>
+      </div>
+
+      <div>
+        <button type="button" data-testid="profile-done-btn">
+          Done Recipes
+        </button>
+        <button type="button" data-testid="profile-favorite-btn">
+          Favorite Recipes
+        </button>
+        <button type="button" data-testid="profile-logout-btn">
+          Logout
+        </button>
+      </div>
+
       <Footer />
-    </>
+    </section>
   );
 }
