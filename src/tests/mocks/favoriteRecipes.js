cdfdee21e4b97bd0c7719b8d80c575871e43460a
Pedroscsilva <pedroscsilva2001@gmail.com>
2022-10-27 17:41:01 -0300

    testes ok, requisto 55 nok

---
+++ src/tests/mocks/favoriteRecipes.js
@@ -0,0 +1,14 @@
+const favoriteRecipes = [
+  {
+    id: '13501',
+    type: 'drink',
+    nationality: '',
+    alcoholicOrNot: 'Alcoholic',
+    category: 'Shot',
+    name: 'ABC',
+    image:
+      'https://www.thecocktaildb.com/images/media/drink/tqpvqp1472668328.jpg',
+  },
+];
+
+export default favoriteRecipes;
