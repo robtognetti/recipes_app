61f41ca7d212de2e9f38c933dad90dbb53eaa8e8
Pedroscsilva <pedroscsilva2001@gmail.com>
2022-10-27 16:30:16 -0300

    req 54

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HorizontalCard from '../components/HorizontalCard';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 export default function FavoriteRecipes() {
-  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+  useEffect(() => {
+    const getFavoriteRecipes = () => {
+      const favoriteRecipesArray = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      setFavoriteRecipes(favoriteRecipesArray);
+    };
+    getFavoriteRecipes();
+  }, []);
+
+  const handleRemoveFavorite = (recipeId) => {
+    const oldArray = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const newArray = oldArray.filter((obj) => obj.id !== recipeId);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(newArray));
+    setFavoriteRecipes(newArray);
+  };
 
   return (
     <section>
@@ -21,15 +36,16 @@ export default function FavoriteRecipes() {
 
       <main>
         {favoriteRecipes?.map((recipe, index) => (
-          <>
+          <div key={ index }>
             <HorizontalCard index={ index } key={ recipe.id } recipe={ recipe } />
             <img
               src={ blackHeartIcon }
               alt="favoritar receita"
               data-testid={ `${index}-horizontal-favorite-btn` }
+              onClick={ () => handleRemoveFavorite(recipe.id) }
               role="presentation"
             />
-          </>
+          </div>
         ))}
       </main>
 
